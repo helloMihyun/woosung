99afb1602daddac3e355c401b5cb6d7e32a429ab
MiHyun <sky1318i@gmail.com>
2020-05-28 17:59:17 +0900

    Add files via upload

--- static/js/comm.js
+++ static/js/comm.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-    $('#gnbMenu').on('mouseover',function(){
+    $('#gnbMenu .depth_1 > a').on('mouseover',function(){
         // alert('f');
+        $('.depth_2').addClass('active');
+    });
+    $('#gnbMenu ').on('mouseleave',function(){
+        // alert('f');
+        $('.depth_2').removeClass('active');
     });
 });
